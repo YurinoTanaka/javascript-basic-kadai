49440a104e2324f8a996b90ecfbb4a646f985270
YurinoTanaka <y.chn616@gmail.com>
2025-01-28 17:25:56 +0900

    初回コミット

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const date = new Date();
+
+console.log(
+  // Dateクラスから'年'取得
+  date.getFullYear() + '年' + 
+  // Dateクラスから'月'取得
+  date.getMonth() + 1 + '月' + 
+  // Dateクラスから'日'取得
+  date.getDate() + '日');
